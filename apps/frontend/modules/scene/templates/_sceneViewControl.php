b39da4772bd69e2d81678092c2551fbed1495b23
Maxim Lakeev <maxim.sergeevich@gmail.com>
2012-02-17 23:17:36 +0000

    fb button rerender

--- apps/frontend/modules/scene/templates/_sceneViewControl.php
+++ apps/frontend/modules/scene/templates/_sceneViewControl.php
@@ -1,7 +1,7 @@
 <?php foreach($scene_times as $scene_time):?>
 	<span id="scene_<?php echo $scene_time['id']?>"<?php if($scene_time['id'] == $scene_id):?> class="active"<?php endif;?>>
 			<?php echo jq_link_to_remote(date('i:s', mktime(0, 0, $scene_time['scene_time'])), array(
-				'update' => array('success_callback' => 'sceneChange(data);', 'failure' => "alert('HTTP Error ' + XMLHttpRequest.status + '!')"),
+				'update' => array('success_callback' => 'sceneChange(data); FB.XFBML.parse();', 'failure' => "alert('HTTP Error ' + XMLHttpRequest.status + '!')"),
 				'url'    => '@scene_change?scene_id='.$scene_time['id'],
 				'method' => 'GET',
 				'condition' => 'checkCurrentScene('.$scene_time['id'].', "'.$scene_time['scene_time'].'")',

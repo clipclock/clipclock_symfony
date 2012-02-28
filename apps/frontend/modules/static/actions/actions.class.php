196e6a5be226e4dc5af08b2af5945ce9d1b9a35c
madesst <madesst@gmail.com>
2012-02-28 20:40:29 +0400

    Правки по загрузке нового видео, определение source

--- apps/frontend/modules/static/actions/actions.class.php
+++ apps/frontend/modules/static/actions/actions.class.php
@@ -48,7 +48,8 @@ class staticActions extends sfActions
 				$this->clip_name = $doc->getElementsByTagName("title")->item(0)->nodeValue;
 			}
 
-			$this->source_id = SourcePeer::retrieveByName($this->source_name);
+			$source = SourcePeer::retrieveByName($this->source_name);
+			$this->source_id = $source['id'];
 			//--
 
 			$clip = ClipPeer::retrieveByUrlAndSourceId($this->clip_url, $this->source_id);

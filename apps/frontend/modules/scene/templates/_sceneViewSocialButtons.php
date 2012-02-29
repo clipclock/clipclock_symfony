8587e7df247f1c3333ca0e9bd07de7af2cabd8f6
madesst <madesst@gmail.com>
2012-02-29 14:39:41 +0400

    Попытки поправить кнопку лайк и убрал кол-во репинов для своего видео, refs #24

--- apps/frontend/modules/scene/templates/_sceneViewSocialButtons.php
+++ apps/frontend/modules/scene/templates/_sceneViewSocialButtons.php
@@ -1,10 +1,10 @@
 <div class="likes">
+	<?php if($user->getId() && $user->getId() != $current_user->getId()):?>
     <div class="rep-like">
         <div class="amount">
             <div class="arrow"></div>
             <?php echo $repins_count?>
         </div>
-		<?php if($user->getId() && $user->getId() != $current_user->getId()):?>
         <div class="info">
             <?php if(!ScenePeer::isRepinnedSceneByUser($origin_scene_id, $current_user->getId())) : ?>
                 <?php echo link_to(image_tag("/images/repin.jpg", array('width' => '60', 'height' => '29')), array('sf_route' => 'scene_repin', 'scene_id' => $origin_scene_id), array('id' => 'new_repin')); ?>
@@ -12,11 +12,11 @@
                 <?php echo link_to(image_tag("/images/repin.jpg", array('width' => '60', 'height' => '29')), array('sf_route' => 'scene_unrepin', 'scene_id' => $origin_scene_id), array('id' => 'un_repin')); ?>
             <?php endif; ?>
         </div>
-			<?php slot('repin_modal') ?>
-				<?php include_partial('scene/modalForm', array('form' => $repin_form, 'form_url' => url_for('scene_repin'), 'form_partial' => 'scene/repinFormFields', 'id' => 'new_repin_modal'))?>
-			<?php end_slot(); ?>
-		<?php endif; ?>
     </div>
+	<?php slot('repin_modal') ?>
+		<?php include_partial('scene/modalForm', array('form' => $repin_form, 'form_url' => url_for('scene_repin'), 'form_partial' => 'scene/repinFormFields', 'id' => 'new_repin_modal'))?>
+	<?php end_slot(); ?>
+	<?php endif; ?>
     <div class="rep-like">
         <!--
         <div class="amount">
--- web/js/fb.js
+++ web/js/fb.js
@@ -1,12 +1,13 @@
 
 // Load the SDK Asynchronously
-(function(d){
+$().ready(function(){
+	var d = document;
 	var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
 	if (d.getElementById(id)) {return;}
 	js = d.createElement('script'); js.id = id; js.async = true;
 	js.src = "//connect.facebook.net/en_US/all.js";
 	ref.parentNode.insertBefore(js, ref);
-}(document));
+});
 
 function toggleFBLikeButton(scene_id, user_id, state, url) {
 	$.ajax({

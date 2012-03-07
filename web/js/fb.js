1fbfe45b662f62f05fef8c6a569befdab8d6cd92
madesst <madesst@gmail.com>
2012-03-07 18:06:49 +0400

    fb

--- web/js/fb.js
+++ web/js/fb.js
@@ -22,10 +22,11 @@ function fbHooks(app_id, scene_id, user_id, url)
 	window.fbAsyncInit = function() {
 		FB.init({
 			appId      : app_id,
-			channelUrl : '//viddii.dev/channel.html',
+			channelUrl : '//dev.viddii.com/channel.html',
 			status     : true, // check login status
 			cookie     : true, // enable cookies to allow the server to access the session
-			xfbml      : true  // parse XFBML
+			xfbml      : true, // parse XFBML
+			oauth: true
 		});
 
 		if(scene_id && user_id && url)
@@ -41,18 +42,19 @@ function fbHooks(app_id, scene_id, user_id, url)
 
 		function sendRequestToRecipients() {
 			FB.ui({method: 'apprequests',
-				message: 'My Great Request',
+				message: 'Maksim Ustinov has invited you to join Pinterest, an online pinboard to organize and share your inspirations!',
 				to: '100003277218703'
 			}, function(){console.log('123')});
 		}
 
 		function sendRequestViaMultiFriendSelector() {
 			FB.ui({method: 'apprequests',
-				message: 'My Great Request'
-			}, function(){console.log('123')});
+				message: 'Maksim Ustinov has invited you to join Pinterest, an online pinboard to organize and share your inspirations!',
+				filters: ["app_non_users"]
+			}, function(response){console.log(response)});
 		}
 		$('#fb_invite').click(function(){
-			sendRequestToRecipients();
+			sendRequestViaMultiFriendSelector();
 			return false;
 		});
 	};

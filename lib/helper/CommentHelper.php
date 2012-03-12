7bf3cd071b91eb3283661c2c9805dd9c7785f1e7
madesst <madesst@gmail.com>
2012-03-12 21:08:48 +0400

    Увеличение кол-ва комментов в стикере

--- lib/helper/CommentHelper.php
+++ lib/helper/CommentHelper.php
@@ -42,21 +42,21 @@ function getCommentRatingClass($rating)
 function calculateCommentListLength($unique_comments_count)
 {
 	$return_count = 1;
-	if($unique_comments_count >= 3 && $unique_comments_count < 7)
-	{
-		$return_count = 2;
-	}
-	elseif($unique_comments_count >= 7 && $unique_comments_count <= 20)
+	if($unique_comments_count >= 2 && $unique_comments_count < 7)
 	{
 		$return_count = 3;
 	}
-	elseif($unique_comments_count > 20 && $unique_comments_count <= 50)
+	elseif($unique_comments_count >= 7 && $unique_comments_count <= 20)
 	{
 		$return_count = 4;
 	}
-	elseif($unique_comments_count > 50)
+	elseif($unique_comments_count > 20 && $unique_comments_count <= 50)
 	{
 		$return_count = 5;
+	}
+	elseif($unique_comments_count > 50)
+	{
+		$return_count = 6;
 	}
 
 	return $return_count;

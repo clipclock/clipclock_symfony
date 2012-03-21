ede2fbeab06e549f471a47ad93e2f7c5e1d55139
madesst <madesst@gmail.com>
2012-03-21 15:18:07 +0400

    Эксперименты с OpenGraph

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns:fb="http://ogp.me/ns/fb#" xml:lang="en" lang="en">
+<html xmlns:fb="http://ogp.me/ns/fb#" xmlns:og="http://ogp.me/ns#" xml:lang="en" lang="en">
 <head>
 	<?php include_http_metas() ?>
 	<?php include_metas() ?>

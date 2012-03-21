56b0131da8ec65b811f0d8df7695ac7ee13e8698
madesst <madesst@gmail.com>
2012-03-21 17:10:34 +0400

    Эксперименты с OpenGraph

--- apps/frontend/modules/scene/actions/components.class.php
+++ apps/frontend/modules/scene/actions/components.class.php
@@ -46,7 +46,8 @@ class sceneComponents extends sfComponents
 		$this->response->addMeta('og:description', 'At '.$this->scene_time . ' - '.$this->scene->getText());
 		$this->response->addMeta('og:site_name', 'ClipClock.com');
 
-		$this->response->addMeta('og:type', 'video.other');
+		//$this->response->addMeta('og:type', 'video.other');
+		$this->response->addMeta('og:type', 'clipclock:clip');
 		$this->response->addMeta('og:video', 'http://www.youtube.com/v/'.$this->reclip->getClip()->getUrl().'?enablejsapi=1&playerapiid=ytplayer&start='.$this->scene_time->getSceneTime().'&autoplay=1&version=3&feature=player_embedded&fs=1&rel=0&showsearch=0&showinfo=0');
 		$this->response->addMeta('og:video:type', 'application/x-shockwave-flash');
 		$this->response->addMeta('og:video:width', '398');

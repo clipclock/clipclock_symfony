f62f5fcc87dff830c260ced3cf6824eda437105e
madesst <madesst@gmail.com>
2012-03-24 07:35:28 +0400

    tests

--- apps/frontend/modules/board/templates/_boardClipsList.php
+++ apps/frontend/modules/board/templates/_boardClipsList.php
@@ -3,9 +3,7 @@
 <?php endforeach?>
 <?php include_partial('static/loadingMsg')?>
 <?php if($pager->getNextPage() != $pager->getPage()):?>
-<?php echo $pager->getNextPage().' '.$current_board->getId().' '.$current_user->getNick() ?>
-<?php echo url_for('board_page', array('id' => $current_board->getId(), 'username_slug' => $current_user->getNick(), 'page' => $pager->getNextPage())) ?>"><?php echo $pager->getNext();?>
 <div class="pager_navigation">
-	<a href="<?php echo url_for('board_page', array('id' => $current_board->getId(), 'username_slug' => $current_user->getNick(), 'page' => $pager->getNextPage())) ?>"><?php echo $pager->getNext();?></a>
+	<a href="<?php echo url_for('board_page', array('id' => $current_board->getId(), 'username_slug' => $user->getNick(), 'page' => $pager->getNextPage())) ?>"><?php echo $pager->getNext();?></a>
 </div>
 <?php endif;?>

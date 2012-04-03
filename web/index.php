0a09638a4bbaac62ab277004cbd82c7720b98d16
madesst <madesst@gmail.com>
2012-04-03 04:12:04 +0400

    Проблемы с мемкешом + попытки пофиксить багу с обновлением страницы сцены

--- web/index.php
+++ web/index.php
@@ -2,21 +2,21 @@
 
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
+//
+//$env = 'prod';
+//if(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
+//{
+//	$env = 'dev';
+//}
+//
+//if(isset($_COOKIE['dev']))
+//{
+//	$cookie_info = unserialize($_COOKIE['dev']);
+//	if(isset($cookie_info['key']) && $cookie_info['key'] == '312312')
+//	{
+//		$env = $cookie_info['env'];
+//	}
+//}
 
-$env = 'prod';
-if(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
-{
-	$env = 'dev';
-}
-
-if(isset($_COOKIE['dev']))
-{
-	$cookie_info = unserialize($_COOKIE['dev']);
-	if(isset($cookie_info['key']) && $cookie_info['key'] == '312312')
-	{
-		$env = $cookie_info['env'];
-	}
-}
-
-$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', $env, false);
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', true);
 sfContext::createInstance($configuration)->dispatch();

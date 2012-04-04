8c63d117afaa610a9eeae4df825b4756b1be285b
madesst <madesst@gmail.com>
2012-04-04 17:29:16 +0400

    Страница недоступности сервиса

---
+++ config/unavailable.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<head prefix="og: http://ogp.me/ns# clipclock:
+                  http://ogp.me/ns/apps/clipclock#">
+	<?php include_http_metas() ?>
+	<?php include_metas() ?>
+	<?php include_title() ?>
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<?php include_combined_stylesheets() ?>
+	<?php include_combined_javascripts() ?>
+</head>
+<body>
+<!-- b-erorr  -->
+<div class="b-erorr">
+	<div class="wrap-in">
+		<a href="/" class="er-logo"></a>
+		<ul class="er-txt">
+			<li class="not">Sorry, we are closed untill 12:00 A.M. April 5th.</li>
+			<li>We are moving to the cloud to handle you and millions of your friends!</li>
+		</ul>
+	</div>
+</div>
+<!-- /b-erorr -->
+</body>
+</html>

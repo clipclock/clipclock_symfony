72f95209960ea46ffebbfbea6cfa09ba4d063cda
Alexey Bulygin <alexeyea@gmail.com>
2012-04-06 23:27:09 +0400

    stage

--- apps/frontend/modules/scene/templates/_sceneViewEmbed.php
+++ apps/frontend/modules/scene/templates/_sceneViewEmbed.php
@@ -1,9 +1,11 @@
 <h2><?php echo $reclip->getClip()->getName()?></h2>
 <div class="viewing">
 <div id="scene_embed_video">
-	<span></span>
+	<div id="scene_embed_video_player"></div>
 </div>
 </div>
 <script type="text/javascript">
-	embedClip(<?php echo $scene_time?>, '<?php echo $reclip->getClip()->getUrl()?>', '<?php echo $reclip->getClip()->getSource()->getName()?>', 'true');
+	function onYouTubePlayerAPIReady() {
+		embedClip(<?php echo $scene_time?>, '<?php echo $reclip->getClip()->getUrl()?>', '<?php echo $reclip->getClip()->getSource()->getName()?>', 'true');
+	}
 </script>

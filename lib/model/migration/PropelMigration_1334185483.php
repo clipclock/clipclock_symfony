35724d6e5827244ff3250420cbbe009399330e1f
en0ne <alexeyea@gmail.com>
2012-04-12 03:47:31 +0400

    migration and task for filling duration for clips

---
+++ lib/model/migration/PropelMigration_1334185483.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Data object containing the SQL and PHP code to migrate the database
+ * up to version 1334185483.
+ * Generated on 2012-04-11 23:04:43 
+ */
+class PropelMigration_1334185483
+{
+
+	public function preUp($manager)
+	{
+		// add the pre-migration code here
+	}
+
+	public function postUp($manager)
+	{
+		// add the post-migration code here
+	}
+
+	public function preDown($manager)
+	{
+		// add the pre-migration code here
+	}
+
+	public function postDown($manager)
+	{
+		// add the post-migration code here
+	}
+
+	/**
+	 * Get the SQL statements for the Up migration
+	 *
+	 * @return array list of the SQL strings to execute for the Up migration
+	 *               the keys being the datasources
+	 */
+	public function getUpSQL()
+	{
+		return array (
+  'propel' => '
+ALTER TABLE clip ADD duration INTEGER DEFAULT 0 NOT NULL;
+
+ALTER TABLE clip_archive ADD duration INTEGER DEFAULT 0 NOT NULL;
+',
+);
+	}
+
+	/**
+	 * Get the SQL statements for the Down migration
+	 *
+	 * @return array list of the SQL strings to execute for the Down migration
+	 *               the keys being the datasources
+	 */
+	public function getDownSQL()
+	{
+		return array (
+  'propel' => '
+ALTER TABLE clip DROP COLUMN duration;
+
+ALTER TABLE clip_archive DROP COLUMN duration;
+',
+);
+	}
+
+}
---
+++ lib/task/fillVideoDurationTask.class.php
@@ -0,0 +1,65 @@
+<?php
+
+class fillVideoDurationTask extends sfBaseTask
+{
+	protected function configure()
+	{
+
+		$this->addOptions(array(
+			new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name'),
+			new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+			new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'propel'),
+			// add your own options here
+		));
+
+		$this->namespace        = 'clips';
+		$this->name             = 'fill-duration';
+		$this->briefDescription = '';
+		$this->detailedDescription = <<<EOF
+The [fillVideoDuration|INFO] task fill duration for all clips with youtube as service if duration is 0.
+Call it with:
+
+  [php symfony fillVideoDuration|INFO]
+EOF;
+	}
+
+	protected function execute($arguments = array(), $options = array())
+	{
+		// for long time task
+		set_time_limit(0);
+
+		// initialize the database connection
+		$databaseManager = new sfDatabaseManager($this->configuration);
+		$connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+
+		$url = 'http://gdata.youtube.com/feeds/api/videos/ugEezy2NeRU';
+
+		$results = ClipQuery::create()
+				->filterByDuration(0)
+				->limit(50)
+				->find();
+
+		$doc = new DOMDocument;
+
+		foreach($results as $r){
+
+			$url = "http://gdata.youtube.com/feeds/api/videos/{$r->getUrl()}";
+
+			if (@($doc->load($url))){
+
+				$value = $doc->getElementsByTagNameNS("http://gdata.youtube.com/schemas/2007", "duration")->item(0)->getAttribute('seconds');
+				$r->setDuration($value);
+				$r->save();
+				$this->log("Saved duration for {$r->getUrl()} : $value");
+
+				sleep(1); // a little pause
+
+			} else {
+
+				$this->log("error: can't load document [$url]");
+			}
+		}
+
+		$this->log('Done!');
+	}
+}

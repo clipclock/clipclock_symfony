42b17ce5a8b20b6b2f0076ebaf140adc85f6d68e
madesst <madesst@gmail.com>
2012-04-13 12:44:17 +0400

    Поправил демона

--- lib/daemon/classes/helper/ytb.php
+++ lib/daemon/classes/helper/ytb.php
@@ -17,7 +17,7 @@ $you_tube_base_url = 'http://www.youtube.com/watch?v=';
 function is_ClipExists($filename)
 {
 	if(file_exists($filename.'.flv'))return '.flv';
-	elseif(file_exists($filename.'.mp4'))return '.mp4';
+	elseif(file_exists($filename.'.mp4') || file_exists($filename.'.video'))return '.mp4';
 	elseif(file_exists($filename.'.3gp'))return '.3gp';//will be test
 	//TODO: search file by extension or add search by other video exension
 	else return false;

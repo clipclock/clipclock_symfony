c9a10141073b5904a7fe0e54a98c0e2482b14973
madesst <madesst@gmail.com>
2012-04-13 19:05:00 +0400

    Смена тайтла для лайков

--- apps/frontend/modules/scene/actions/components.class.php
+++ apps/frontend/modules/scene/actions/components.class.php
@@ -66,7 +66,7 @@ class sceneComponents extends sfComponents
 		$this->post_facebook = $this->getRequest()->getCookie('post_facebook', true);
 
 		$this->response->addMeta('og:url', $this->generateUrl('scene', array('username_slug' => $this->user, 'board_id' => $this->scene->getBoardId(), 'id' => $this->scene->getId()), true));
-		$this->response->addMeta('og:title', truncate_text($this->scene->getText(), 140, '…', true).' | Clip your best moment here>>');
+		$this->response->addMeta('og:title', truncate_text($this->scene->getText(), 140, '…', true).' | Clip your great moments here>>');
 		$this->response->addMeta('og:image', $this->generateUrl('homepage', array(), true).substr(ImagePreview::c14n($this->reclip->getClip()->getId().$this->scene_time->getSceneTime(), 'big'), 1));
 		$this->response->addMeta('og:description', $this->reclip->getClip()->getName().' at '.$this->scene_time);
 		$this->response->addMeta('og:site_name', $this->getResponse()->getTitle());

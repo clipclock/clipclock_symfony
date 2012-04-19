0e2041000019a9499f7510e54e2d89cc4c69c1e4
Alexey Bulygin <alexeyea@gmail.com>
2012-04-19 19:04:10 +0400

    Revert "optimizations for js and some fixes"
    This reverts commit d5df10d1114ff7f156dedfe9a4263d249e1fb78c.

--- apps/frontend/modules/home/templates/indexSuccess.php
+++ apps/frontend/modules/home/templates/indexSuccess.php
@@ -18,9 +18,7 @@ $(function(){
 	layoutAndScroll('<?php echo $next_url ?>');
 <?php if ($modal): ?>
 	if ($.browser.msie){
-		<?php if (isset($scene_id)): ?>
 		window.location.href = "<?php echo url_for('redirect_to_scene', array('scene_id' => $scene_id)) ?>";
-		<?php endif ?>
 	} else {
 		stickerClick(0, '<?php echo url_for('scene_change', array('scene_id' => $scene_id, 'modal' => 1)) ?>', '<?php echo $current_url ?>', '<?php echo url_for('scene_change', array('scene_id' => $scene_id, 'modal' => 1)) ?>', 0, '<?php echo $scene_id ?>', '<?php echo $new_user?>');
 	}

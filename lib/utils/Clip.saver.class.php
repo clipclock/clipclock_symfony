0ffce006e1620271f48d0995ed666ef09bff1c8b
madesst <madesst@gmail.com>
2012-04-20 19:41:07 +0400

    Фикс 500 прода, refs #99

--- lib/utils/Clip.saver.class.php
+++ lib/utils/Clip.saver.class.php
@@ -36,51 +36,58 @@ class ClipSaver
 	{
 		$url = "http://gdata.youtube.com/feeds/api/videos/". $clip_url;
 		$doc = new DOMDocument;
-		$doc->load($url);
-		$clip_name = $doc->getElementsByTagName("title")->item(0)->nodeValue;
-		$clip_duration = $doc->getElementsByTagNameNS("http://gdata.youtube.com/schemas/2007", "duration")->item(0)->getAttribute('seconds');
+		$load_result = $doc->load($url);
 
-		if(!$source_id)
+		if($load_result)
 		{
-			$source = SourcePeer::retrieveByName($source_name);
-			$source_id = $source['id'];
-		}
+			$clip_name = $doc->getElementsByTagName("title")->item(0)->nodeValue;
+			$clip_duration = $doc->getElementsByTagNameNS("http://gdata.youtube.com/schemas/2007", "duration")->item(0)->getAttribute('seconds');
 
-		$clip = ClipPeer::retrieveByUrlAndSourceId($clip_url, $source_id);
-
-		if(!$clip)
-		{
-			$clip = new Clip();
-			$clip->setUrl($clip_url);
-			$clip->setName($clip_name);
-			$clip->setSourceId($source_id);
-			$clip->setDuration($clip_duration);
-
-			if(is_array($social_info))
+			if(!$source_id)
 			{
-				$provider = ProviderQuery::create()->findOneByName($social_info['provider']);
-				$clip_social = new ClipSocialInfo();
-				$clip_social->setCreatedAt($social_info['created_at']);
-				$clip_social->setDescription($social_info['description']);
-				$clip_social->setPostId($social_info['post_id']);
-
-				$ext_user = ExtUserQuery::create()->filterByProvider($provider)->findOneByExtId($social_info['ext_user_id']);
-				if(!$ext_user)
-				{
-					$ext_user = new ExtUser();
-					$ext_user->setProvider($provider);
-					$ext_user->setNick($social_info['ext_user_nick']);
-					$ext_user->setExtId($social_info['ext_user_id']);
-					$ext_user->save();
-				}
-
-				$clip_social->setExtUserId($ext_user->getId());
-				$clip_social->save();
-				$clip->setClipSocialInfo($clip_social);
+				$source = SourcePeer::retrieveByName($source_name);
+				$source_id = $source['id'];
 			}
-			$clip->save();
-		}
 
-		return $clip;
+			$clip = ClipPeer::retrieveByUrlAndSourceId($clip_url, $source_id);
+
+			if(!$clip)
+			{
+				$clip = new Clip();
+				$clip->setUrl($clip_url);
+				$clip->setName($clip_name);
+				$clip->setSourceId($source_id);
+				$clip->setDuration($clip_duration);
+
+				if(is_array($social_info))
+				{
+					$provider = ProviderQuery::create()->findOneByName($social_info['provider']);
+					$clip_social = new ClipSocialInfo();
+					$clip_social->setCreatedAt($social_info['created_at']);
+					$clip_social->setDescription($social_info['description']);
+					$clip_social->setPostId($social_info['post_id']);
+
+					$ext_user = ExtUserQuery::create()->filterByProvider($provider)->findOneByExtId($social_info['ext_user_id']);
+					if(!$ext_user)
+					{
+						$ext_user = new ExtUser();
+						$ext_user->setProvider($provider);
+						$ext_user->setNick($social_info['ext_user_nick']);
+						$ext_user->setExtId($social_info['ext_user_id']);
+						$ext_user->save();
+					}
+
+					$clip_social->setExtUserId($ext_user->getId());
+					$clip_social->save();
+					$clip->setClipSocialInfo($clip_social);
+				}
+				$clip->save();
+			}
+
+			return $clip;
+		}
+		else{
+			throw new Exception('Undefined youtube video');
+		}
 	}
 }
